8f4a50e468c540bbf19455998f3717b6109132ab
Alicia de Mora Losana <aliciademoralosana@yahoo.com>
2024-04-18 20:18:37 +0100

    Add distinction between published, scheduled and draft posts, showing only published to non-signed in users

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,8 +1,9 @@
 class BlogPostsController < ApplicationController
     before_action :authenticate_user!, except: [:index, :show]
-    before_action :set_blog_post, only: [:show, :edit, :destroy]
+    before_action :set_blog_post, except: [:index, :new, :create]
+
     def index
-        @blog_posts = BlogPost.all
+        @blog_posts = user_signed_in? ? BlogPost.sorted : BlogPost.published.sorted
     end
 
     def show
@@ -39,13 +40,13 @@ class BlogPostsController < ApplicationController
 
     private
 
-    def set_blog_post
-        @blog_post = BlogPost.find(params[:id])
-    rescue ActiveRecord::RecordNotFound
-        redirect_to root_path
+    def blog_post_params
+        params.require(:blog_post).permit(:title, :body, :published_at)
     end
 
-    def blog_post_params
-        params.require(:blog_post).permit(:title, :body)
+    def set_blog_post
+        @blog_post = user_signed_in? ? BlogPost.find(params[:id]) : BlogPost.published.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+        redirect_to root_path
     end
 end
