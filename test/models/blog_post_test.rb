f54e2bf38fa0924e81fb4b27504576c8fe2f114a
Alicia de Mora Losana <aliciademoralosana@yahoo.com>
2024-04-18 20:51:34 +0100

    Add testing blog posts published draft scheduled methods

--- test/models/blog_post_test.rb
+++ test/models/blog_post_test.rb
@@ -1,7 +1,40 @@
 require "test_helper"
 
 class BlogPostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "draft? is true for unpublished posts where user has selected blank for datetime_select" do
+    assert blog_posts(:draft).draft?
+  end
+
+  test "draft? is false for published posts" do
+    refute blog_posts(:published).draft?
+  end
+
+  test "draft? is false for scheduled posts" do
+    refute blog_posts(:scheduled).draft?
+  end
+
+  test "published? is true for published posts" do
+    assert blog_posts(:published).published?
+  end
+
+  test "published? is false for scheduled posts" do
+    refute blog_posts(:scheduled).published?
+  end
+
+  test "published? is false for draft posts" do
+    refute blog_posts(:draft).published?
+  end
+
+  test "scheduled? is true for scheduled posts" do
+    assert blog_posts(:scheduled).scheduled?
+  end
+
+  test "scheduled? is false for draft posts" do
+    refute blog_posts(:draft).scheduled?
+  end
+
+  test "scheduled? is false for published posts" do
+    refute blog_posts(:published).scheduled?
+  end
+
 end
