ac000d9b018ea911aa7601182b94a345580419e0
Alicia de Mora Losana <aliciademoralosana@yahoo.com>
2024-04-20 15:05:35 +0100

    Limit number of blog posts visible per page to 3

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -4,7 +4,7 @@ class BlogPostsController < ApplicationController
 
     def index
         @blog_posts = user_signed_in? ? BlogPost.sorted : BlogPost.published.sorted
-        @pagy, @blog_posts = pagy(@blog_posts)
+        @pagy, @blog_posts = pagy(@blog_posts, items: 3)
     rescue Pagy::OverflowError
         redirect_to root_path(page: 1)
     end
