f8253d131e55b5a5c30058e89b5515655d618341
Alicia de Mora Losana <aliciademoralosana@yahoo.com>
2024-04-20 19:18:55 +0100

    Add sample posts as seeds through JSON file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,6 @@
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
 
 user = User.where(email: "aliciademoralosana@yahoo.com").first_or_initialize
 user.update!(
@@ -14,7 +8,9 @@ user.update!(
     password_confirmation: "password"
 )
 
-100.times do |i|
-    blog_post = BlogPost.where(title: "Blog Post #{i}").first_or_initialize
-    blog_post.update(content: "Let's f***ing go!", published_at: Time.current)
-end
+blog_posts_data = JSON.parse(File.read(Rails.root.join('db', 'seed_posts.json')))
+
+blog_posts_data.each_with_index do |data, i|
+    blog_post = BlogPost.where(title: data['title']).first_or_initialize
+    blog_post.update!(content: data['content'], published_at: Time.current)
+end  
