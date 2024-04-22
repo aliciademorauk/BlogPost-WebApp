f2eba39615058ce0053e9f96cbf77023c5ba9d3e
Alicia de Mora Losana <aliciademoralosana@yahoo.com>
2024-04-22 18:36:18 +0100

    Update seeds for initial password to be generated with secure random number generator

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,15 +2,22 @@
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 
-user = User.where(email: "aliciademoralosana@yahoo.com").first_or_initialize
+require 'securerandom'
+
+random_password = SecureRandom.hex(8)
+# Add your email address below:
+email = "aliciademoralosana@yahoo.com"
+
+user = User.where(email: email).first_or_initialize
 user.update!(
-    password: "password",
-    password_confirmation: "password"
+    password: random_password,
+    password_confirmation: random_password
 )
 
-blog_posts_data = JSON.parse(File.read(Rails.root.join('db', 'seed_posts.json')))
+puts "Random password generated for user '#{email}': #{random_password}"
 
-blog_posts_data.each_with_index do |data, i|
+blog_posts_data = JSON.parse(File.read(Rails.root.join('db', 'seed_posts.json')))
+blog_posts_data.each do |data|
     published_at_value = data['published_at'].present? ? DateTime.parse(data['published_at']) : nil
     blog_post = BlogPost.where(title: data['title']).first_or_initialize
     blog_post.update!(
@@ -18,3 +25,5 @@ blog_posts_data.each_with_index do |data, i|
         published_at: published_at_value
     )
 end
+
+
